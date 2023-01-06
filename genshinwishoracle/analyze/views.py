69d4c61ee8c60f64f1735806acef2c43afc18a6b
Charlie DiLorenzo <charlie.dilorenzo64@gmail.com>
2023-01-06 12:52:02 -0500

    basic ui for analyze url, adding banner creation

--- genshinwishoracle/analyze/views.py
+++ genshinwishoracle/analyze/views.py
@@ -1,36 +1,55 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
+# from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 from django.views import View
+from django.urls import reverse_lazy
 
-# from .models import 
+from .models import Character, Weapon, CharacterBanner, WeaponBanner
 
 
 class IndexView(generic.ListView):
-    # TODO
     template_name = 'analyze/index.html'
+    context_object_name = 'suburls'
+
+    def get_queryset(self):
+        list = ["analyze:characterbanner", "analyze:weaponbanner", "analyze:analyzestatistics"]
+        return list
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     return context
+
+class CharacterBannerView(generic.ListView):
+    # TODO
+    template_name = 'analyze/characterbanner.html'
+    context_object_name = 'character_banners'
+
+    def get_queryset(self):
+        banners = CharacterBanner.objects.order_by()
+        print("HERE", banners[0].rateups)
+        # question = get_object_or_404(Question, pk=question_id)
+        return banners
+    # characters = Character.objects.
+    # template_name = 'analyze/index.html'
     # context_object_name = 
+    pass
 
-    # def get_queryset(self):
-    #     """
-    #     Return the last five published questions (not including those set to be
-    #     published in the future).
-    #     """
-    #     return Question.objects.filter(
-    #         pub_date__lte=timezone.now()
-    #     ).order_by('-pub_date')[:5]
+class CharacterBannerCreateView(generic.CreateView):
+    model = CharacterBanner
+    fields = ('name', 'rateups')
+    success_url = reverse_lazy('analyze:characterbanner')
+    template_name = 'analyze/characterbannercreate.html'
 
-class AnalyzeCharacterBannerView(generic.ListView):
+
+class WeaponBannerView(generic.TemplateView):
     # TODO
     # template_name = 'analyze/index.html'
     # context_object_name = 
     pass
 
-class AnalyzeWeaponBannerView(generic.ListView):
+class StatisticsAnalyzeView(generic.TemplateView):
     # TODO
-    # template_name = 'analyze/index.html'
-    # context_object_name = 
     pass
