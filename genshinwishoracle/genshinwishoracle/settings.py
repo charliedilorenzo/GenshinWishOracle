e7b86997cf95c0e43ffa8297e837a62c8bdca99f
Charlie DiLorenzo <charlie.dilorenzo64@gmail.com>
2023-01-23 16:18:28 -0500

    proper static settings

--- genshinwishoracle/genshinwishoracle/settings.py
+++ genshinwishoracle/genshinwishoracle/settings.py
@@ -113,13 +113,19 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_ROOT = BASE_DIR / "static"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    "C:\\Users\\carol\\Code\\Personal\\GenshinWishOracle\\genshinwishoracle\\analyze\\static\\analyze",
+    "C:/Users/carol/Code/Personal/GenshinWishOracle/genshinwishoracle/analyze/static/analyze",
+]
