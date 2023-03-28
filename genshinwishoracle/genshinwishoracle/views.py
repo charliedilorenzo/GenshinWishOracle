9f5cb49ab69dda79f464556cc2a83b371f64f1f8
Charlie DiLorenzo <charlie.dilorenzo64@gmail.com>
2023-03-27 20:45:06 -0400

    fix create

--- genshinwishoracle/genshinwishoracle/views.py
+++ genshinwishoracle/genshinwishoracle/views.py
@@ -36,6 +36,7 @@ class CharacterBannerView(generic.ListView):
     template_name = 'analyze/banners_list.html'
     context_object_name = 'banners'
     back_url = reverse_lazy('main-home')
+    create_url = reverse_lazy('character_banner_create')
     base_url = "analyze"
     banner_type = "Character"
     # TODO see if I want this
@@ -47,6 +48,7 @@ class CharacterBannerView(generic.ListView):
         context[self.context_object_name] = character_banners
         context['labels'] = ["Name", "Enddate", "5⭐ Rateup", "4⭐ Rateup 1", "4⭐ Rateup 2", "4⭐ Rateup 3", "Edit", "Delete"]
         context['back_url'] = self.back_url
+        context['create_url'] = self.create_url
         context['url_front'] = "/character-banners"
         context['banner_type'] = self.banner_type
         return context
@@ -161,6 +163,7 @@ class WeaponBannerView(generic.ListView):
     template_name = 'analyze/banners_list.html'
     context_object_name = 'banners'
     back_url = reverse_lazy('main-home')
+    create_url = reverse_lazy('weapon_banner_create')
     banner_type = "Weapon"
     
     def get_context_data(self, **kwargs):
@@ -169,6 +172,7 @@ class WeaponBannerView(generic.ListView):
         context[self.context_object_name] = weaponbanners
         context['labels'] = ["Name", "Enddate", "5⭐ Rateup 1","5⭐ Rateup 2", "4⭐ Rateup 1", "4⭐ Rateup 2", "4⭐ Rateup 3", "4⭐ Rateup 4", "4⭐ Rateup 5", "Edit", "Delete"]
         context['back_url'] = self.back_url
+        context['create_url'] = self.create_url
         context['url_front'] = "/weapon-banners"
         context['banner_type'] = self.banner_type
         return context 
